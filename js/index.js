64f6c3ae6dcdd199d4212868c0197f7309a29c4d
RF-code <rfan23@punaou.edu>
2016-06-30 14:28:17 -1000

    Created the street!
    Created the randomizer streets, and added two buildings and a park.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,27 @@
-var = colors 
+var colors =
 	[
 	"red",
 	"yellow",
 	"green"
 	]
 
-function() trafficColor{
-	
+var trafficColor = function(streetPart){
+	var randomColor = Math.floor(Math.random()* colors.length);
+
+	document.getElementById(streetPart).style.backgroundColor = colors[randomColor];
+}
+
+var display = function(){
+	trafficColor("streetA1");
+	trafficColor("streetA2");
+	trafficColor("streetA3");
+	trafficColor("streetA4");
+	trafficColor("streetB1");
+	trafficColor("streetB2");
+	trafficColor("streetB3");
+	trafficColor("streetB4");
+	trafficColor("streetB5");
+	trafficColor("streetB6");
+	trafficColor("streetB7");
+	trafficColor("streetB8");
 }
